44b0186e9afd2ed9e66126a0ddf231f552eed07c
audreynoy <131901124+audreynoy@users.noreply.github.com>
2023-06-14 13:10:46 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,56 @@
+function valideclicked(){
+    alert('inscription faite avec succes')
+}
+function champ(){
+    var TVA =document.getElementById("TVA")
+    if(TVA.Value.length ==0){
+        alert ("pas de TVA ")
+    }
+
+    function champ(){
+        var nom=document.getElementById("nom")
+        if(nom.Value.length ==0){
+            alert ("pas de nom")
+        }}
+        function champ(){
+            var sexe =document.getElementById("sexe")
+            if(sexe.Value.length ==0){
+                alert ("pas de sexe")
+            }
+        }
+            function champ(){}
+                var adresse =document.getElementById("adresse")
+                if(adresse.Value.length ==0){
+                    alert ("pas d'adresse")
+                }
+            }
+                function champ(){
+                    var adresse =document.getElementById("adresse")
+                    if(adresse.Value.length ==0){
+                        alert ("pas d'adresse")}
+                    }
+
+                        function champ(){
+                            var codepostal =document.getElementById("code postal")
+                            if(codepostal.Value.length ==0){
+                                alert ("pas de code postal")
+                            }
+                        }
+                            function champ(){
+                                var region =document.getElementById("region")
+                                if(region.Value.length ==0){
+                                    alert ("pas de region")
+                                }
+                            }
+                                function champ(){
+                                    var cartedepaiement =document.getElementById("region")
+                                    if(region.Value.length ==0){
+                                        alert ("pas de carte")
+                                    }
+                                }
+                                    function champ(){
+                                        var fournisseur =document.getElementById("fournisseur")
+                                        if(fournisseur.Value.length ==0){
+                                            alert ("pas de fournisseur")
+                                        }}
+                    
